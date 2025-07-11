5ed53378c4caa11afdc7a91ccd8309839fdfe41a
ahmadjon09 <youtobe83a61a@gmail.com>
2025-07-11 21:38:06 +0500

    B

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -3,8 +3,8 @@ import Cookies from 'js-cookie'
 
 const is_auth = Cookies.get('client_token')
 
-// const BASE_URL = 'https://b-market-server.onrender.com/api'
-const BASE_URL = 'http://localhost:8000/api/'
+const BASE_URL = 'https://b-market-server.onrender.com/api/'
+// const BASE_URL = 'http://localhost:8000/api/'
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
